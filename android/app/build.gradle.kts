85a07ab14bdb1e2d3b55ba6448ecf02850a4c939
KaivanKeren <x.iml.7122@gmail.com>
2026-08-03 11:24:02 +0700

    chore: enable core library desugaring for java.time support

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -12,6 +12,9 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
+        // flutter_local_notifications schedules with java.time, which needs
+        // desugaring on the minSdk levels we still support.
+        isCoreLibraryDesugaringEnabled = true
     }
 
     defaultConfig {
@@ -42,4 +45,8 @@ kotlin {
 
 flutter {
     source = "../.."
+}
+
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.5")
 }
